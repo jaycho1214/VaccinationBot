6da9c3b76c28d1f14467adc8b69b008178887a50
Travis Cho <chotravis87@gmail.com>
2021-07-16 10:49:20 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# 코로나 백신 사전예약 봇
+
+**필수**: 크롬 브라우저 및 파이썬이 꼭 설치 되어 있어야 합니다!
+**참고**: 휴대폰 본인인증에 있는 자동입력방지 문구와 검사할 곳은 직접 입력하셔야 합니다!
+
+## 사용방법
+*파이썬 3.9버전 미만은 작동 여부를 확인하지 않았습니다*
+
+1. `pip3 install -r requirements.txt` 또는
+`conda env create -f environment.yml && conda activate VaccinationBot`
+
+2. `target`폴더에 있는 `template.ini`를 본인에 맞게 변경하시면 됩니다. 혹시 여려명을 동시에 할 경우 `template.ini`파일을 복사해서 각각 변경하시면 됩니다.
+
+3. `python3 run_vaccinate.py` 
+
+## Requirements
+* [Selenium](https://pypi.org/project/selenium/)
+* [chromedriver_autoinstaller](https://pypi.org/project/chromedriver-autoinstaller/)
+
+## 문제제기
+이슈나 제안할 점은 여기로 제보해주시기 바랍니다 :)
+[이슈 제보하기](https://github.com/MO-Ventures/VaccinationBot/issues)
---
+++ run_vaccinate.py
@@ -0,0 +1,110 @@
+import configparser
+import glob
+from pathlib import Path
+from multiprocessing import Process
+import logging
+import chromedriver_autoinstaller
+import selenium
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+URL = 'https://ncvr.kdca.go.kr/'
+FIRST_PAGE_TIMEOUT = 5 * 60 * 60 # 처음 로딩할때 기다리는 시간 (대기하는거 포함)
+TIMEOUT = 30 # 로딩된 이후 기다리는 시간
+
+def load_main_page(driver: webdriver) -> bool:
+    driver.get(URL)
+    logging.debug('Webpage is being loaded')
+    element1_present = expected_conditions.presence_of_element_located((By.XPATH, '//frame[@src="/cobk/index.jsp"]'))
+    element2_clickable = expected_conditions.presence_of_element_located((By.XPATH, '//div[contains(@class, "banner01")]'))
+    try:
+        WebDriverWait(driver, FIRST_PAGE_TIMEOUT).until(element1_present)
+        logging.debug('First Waiting Done')
+        driver.switch_to.frame(driver.find_element(By.XPATH, '//frame[@src="/cobk/index.jsp"]'))
+        logging.debug('Switch frame successfully')
+        WebDriverWait(driver, FIRST_PAGE_TIMEOUT).until(element2_clickable)
+        logging.debug('Second Waiting Done')
+        return True
+    except:
+        return False
+
+def privacy(driver: webdriver, config: dict) -> bool:
+    # 사이트가 완전히 로딩되었는지 확인
+    element1_present = expected_conditions.presence_of_element_located((By.ID, 'ptntRrn2'))
+    try:
+        WebDriverWait(driver, TIMEOUT).until(element1_present)
+        # 카카오톡
+        driver.find_element(By.ID, 'smsRcptAgrmYn2').click()
+        # 개인정보 채우기
+        driver.find_element(By.ID, 'patnam').send_keys(config['피접종자']['이름'])
+        driver.find_element(By.ID, 'ptntRrn1').send_keys(config['피접종자']['주민등록번호'])
+        driver.find_element(By.ID, 'ptntRrn2').send_keys(config['피접종자']['주민등록번호'])
+        driver.find_element(By.ID, 'apnmNm').send_keys(config['대리인']['이름'])
+        driver.find_element(By.ID, 'apnmMtnoTofmn').send_keys(config['대리인']['휴대폰번호'].split('-')[0])
+        driver.find_element(By.ID, 'apnmMtno1').send_keys(config['대리인']['휴대폰번호'].split('-')[1])
+        driver.find_element(By.ID, 'apnmMtno2').send_keys(config['대리인']['휴대폰번호'].split('-')[2])
+        driver.find_element(By.ID, 'apnmRrn1').send_keys(config['대리인']['주민등록번호'].split('-')[0])
+        driver.find_element(By.ID, 'apnmRrn2').send_keys(config['대리인']['주민등록번호'].split('-')[1])
+        return True
+    except:
+        return False
+
+def authenticate(driver: webdriver, config: dict) -> None:
+    # 사이트가 완전히 로딩되었는지 확인
+    element1_present = expected_conditions.presence_of_element_located((By.XPATH, '//a[@href="#kt"]'))
+    try:
+        WebDriverWait(driver, TIMEOUT).until(lambda driver: len(driver.window_handles) == 2)
+        driver.switch_to.window(driver.window_handles[1])
+        WebDriverWait(driver, TIMEOUT).until(element1_present)
+        driver.find_element(By.XPATH, '//a[@href="#kt"]').click()
+        driver.find_element(By.XPATH, '//a[@class="tabApp"]').click()
+        driver.find_element(By.ID, 'nm').send_keys(config['피접종자']['이름'])
+        driver.find_element(By.ID, 'mbphn_no').send_keys(''.join(config['대리인']['휴대폰번호'].split('-')))
+        driver.find_element(By.ID, 'certi01').click()
+        driver.find_element(By.ID, 'certi02').click()
+        driver.find_element(By.ID, 'certi03').click()
+        driver.find_element(By.ID, 'certi04').click()
+        # driver.switch_to.window(driver.window_handles[0])
+        # driver.switch_to.frame(driver.find_element(By.XPATH, '//frame[@src="/cobk/index.jsp"]'))
+    except:
+        return False
+
+def main(filename: str) -> None:
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    # 화면 보일때 까지 계속 로딩하기
+    while not load_main_page(driver):
+        pass
+
+    # 백신 예약 버튼 클릭
+    driver.find_element(By.XPATH, '//div[contains(@class, "banner01")]').click()
+
+    # 대리 예약 버튼 클릭
+    driver.find_element(By.ID, 'rsrvAgntBtn').click()
+
+    # 개인정보입력
+    privacy(driver, config)
+
+    # 휴대폰 본인인증 클릭
+    driver.find_element(By.XPATH, '//div[contains(@class, "auth01")]').click()
+
+    # 본인인증하기
+    authenticate(driver, config)
+
+    # 창 닫으면 프로그램 종료
+    WebDriverWait(driver, TIMEOUT).until(lambda driver: len(driver.window_handles) == 0) 
+    driver.quit()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    target = glob.glob(str(Path(__file__).parent / 'target' / '*.ini'))
+    p = [Process(target=main, args=(t,)) for t in target]
+    [_p.start() for _p in p]
+    [_p.join() for _p in p]
